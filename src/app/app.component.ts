562e61f853fe6cc63d604c3c3c4729b8ac1e5528
Ashutosh Singh <ashu.singh100@gmail.com>
2020-03-28 15:48:34 +0530

    XML to Json to Excel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { NgxXml2jsonService } from 'ngx-xml2json';
+import * as _ from 'lodash';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  satiClearingForm: FormGroup;
   title = 'Sati-Clearing-Agency';
+  sanitizeObjects = [];
+
+  constructor(private _fb: FormBuilder, private ngxXml2jsonService: NgxXml2jsonService, private appService: AppService) {
+  }
+
+  ngOnInit() {
+    this.satiClearingForm = this._fb.group({
+      xmlInput: []
+    })
+  }
+
+  addTable() {
+    const rawXml = this.satiClearingForm.get("xmlInput").value
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(rawXml, 'text/xml');
+    const rawObj = this.ngxXml2jsonService.xmlToJson(xml);
+    this.sanitizeObject(rawObj);
+  }
+
+  sanitizeObject(rawObj) {
+    let sanitizeObject = {};
+    sanitizeObject["Number_of_loading_lists"] = _.get(rawObj, ["ASYCUDA", "Property", "Nbers", "Number_of_loading_lists"]);
+    sanitizeObject["Serial_number"] = _.get(rawObj, ["ASYCUDA", "Identification", "Registration", "Number"]);
+    sanitizeObject["Value_details"] = _.get(rawObj, ["ASYCUDA", "General_information", "Value_details"]);
+    sanitizeObject["Total_CIF"] = _.get(rawObj, ["ASYCUDA", "Valuation", "Total_CIF"]);
+    sanitizeObject["Total_number_of_packages"] = _.get(rawObj, ["ASYCUDA", "Property", "Nbers", "Total_number_of_packages"]);
+    sanitizeObject["Customs_clearance_office_code"] = _.get(rawObj, ["ASYCUDA", "Identification", "Office_segment", "Customs_clearance_office_code"]);
+    sanitizeObject["Consignee_code"] = _.get(rawObj, ["ASYCUDA", "Traders", "Consignee", "Consignee_code"]);
+    sanitizeObject["Total_invoice"] = _.get(rawObj, ["ASYCUDA", "Valuation", "Total", "Total_invoice"]);
+    sanitizeObject["Previous_document_reference"] = _.get(rawObj, ["ASYCUDA", "Item", 0, "Previous_doc", "Previous_document_reference"]);
+    this.sanitizeObjects.push(sanitizeObject);
+  }
+
+  downloadTable() {
+    let excelData = [];
+    this.sanitizeObjects.forEach(sanitizeObject => {
+      excelData.push(
+        [
+          sanitizeObject["Number_of_loading_lists"], 
+          sanitizeObject["Serial_number"], 
+          sanitizeObject["Value_details"], 
+          sanitizeObject["Total_CIF"], 
+          sanitizeObject["Total_number_of_packages"], 
+          sanitizeObject["Customs_clearance_office_code"], 
+          sanitizeObject["Consignee_code"], 
+          sanitizeObject["Total_invoice"], 
+          sanitizeObject["Previous_document_reference"]])
+    });
+    this.appService.generateExcel(excelData);
+  }
+
 }
